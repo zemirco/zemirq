6a89ccf69890361670430af1479900a05e2e6d53
Daniel Pfeffer <daniel.pfeffer@hbm.com>
2020-01-20 17:26:43 +0100

    parallel PUBSUB and REQREP on 127.0.0.1:8081

--- main.go
+++ main.go
@@ -2,16 +2,57 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"nanomsg.org/go/mangos/v2"
 	"nanomsg.org/go/mangos/v2/protocol/pub"
+	"nanomsg.org/go/mangos/v2/protocol/rep"
 	"nanomsg.org/go/mangos/v2/transport/ws"
 )
 
+func reqHandler(sock mangos.Socket) {
+	fmt.Println("repHandler running.")
+	var err error
+	var msg []byte
+
+	for {
+		fmt.Println("waiting for REQ")
+		// Could also use sock.RecvMsg to get header
+		msg, _ = sock.Recv()
+		fmt.Println("parsing REQ")
+		if string(msg) == "DATE" { // no need to terminate
+			fmt.Println("RECEIVED DATE REQUEST")
+			d := date()
+			fmt.Printf("SENDING DATE %s\n", d)
+			err = sock.Send([]byte(d))
+			if err != nil {
+				die("can't send reply: %s", err.Error())
+			}
+		} else if string(msg) == "GREET" { // no need to terminate
+			fmt.Println("RECEIVED GREET REQUEST")
+			d := "Hello, requester!"
+			fmt.Printf("SENDING GREET %s\n", d)
+			err = sock.Send([]byte(d))
+			if err != nil {
+				die("can't send reply: %s", err.Error())
+			}
+		}
+	}
+}
+
+func die(format string, v ...interface{}) {
+	fmt.Fprintln(os.Stderr, fmt.Sprintf(format, v...))
+	os.Exit(1)
+}
+
+func date() string {
+	return time.Now().Format(time.ANSIC)
+}
+
 func subHandler(sock mangos.Socket) {
+	fmt.Println("subHandler running.")
 	count := 0
 	for {
 		msg := fmt.Sprintf("PUB #%d %s", count, time.Now().String())
@@ -24,29 +65,48 @@ func subHandler(sock mangos.Socket) {
 }
 
 func main() {
-	fmt.Println("awesome")
+	fmt.Println("awesome, let's go!")
 
 	sock, err := pub.NewSocket()
 	if err != nil {
 		panic(err)
 	}
 
-	listener, err := sock.NewListener("ws://localhost:3000/sub", nil)
+	sublistener, err := sock.NewListener("ws://127.0.0.1:8081/sub", nil)
 	if err != nil {
 		panic(err)
 	}
 
-	h, err := listener.GetOption(ws.OptionWebSocketHandler)
+	subh, err := sublistener.GetOption(ws.OptionWebSocketHandler)
 	if err != nil {
 		panic(err)
 	}
 
-	http.Handle("/sub", h.(http.Handler))
-	listener.Listen()
-
+	http.Handle("/sub", subh.(http.Handler))
+	sublistener.Listen()
 	go subHandler(sock)
 
-	http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("public"))))
+	repsock, err := rep.NewSocket()
+	if err != nil {
+		panic(err)
+	}
 
-	log.Fatal(http.ListenAndServe(":3000", nil))
+	replistener, err := repsock.NewListener("ws://127.0.0.1:8081/req", nil)
+	if err != nil {
+		panic(err)
+	}
+
+	reph, err := replistener.GetOption(ws.OptionWebSocketHandler)
+	if err != nil {
+		panic(err)
+	}
+
+	http.Handle("/req", reph.(http.Handler))
+	replistener.Listen()
+	go reqHandler(repsock)
+
+	http.Handle("/", http.FileServer(http.Dir("public")))
+
+	e := http.ListenAndServe(":8081", nil)
+	die("Http server died: %v", e)
 }
--- public/script.js
+++ public/script.js
@@ -2,20 +2,81 @@
 console.log('app started')
 
 // const socket = new WebSocket('ws://localhost:3000/sub', 'pub.sp.nanomsg.org')
-const socket = new WebSocket('ws://localhost:3000/sub', ['rep.sp.nanomsg.org', 'pub.sp.nanomsg.org'])
-socket.binaryType = 'arraybuffer'
+const subsocket = new WebSocket('ws://127.0.0.1:8081/sub', ['pub.sp.nanomsg.org'])
+subsocket.binaryType = 'arraybuffer'
 
-socket.onerror = (event) => {
-  console.log('error')
+const repsocket = new WebSocket('ws://127.0.0.1:8081/req', ['rep.sp.nanomsg.org'])
+repsocket.binaryType = 'arraybuffer'
+
+var enc = new TextEncoder(); // always utf-8
+var dec = new TextDecoder(); // always utf-8
+
+function reqDATE() {
+  console.log('Sending REP message: DATE')
+  makeRequest("DATE")
+}
+
+function reqGREET() {
+  console.log('Sending REP message: DATE')
+  makeRequest("GREET")
+}
+
+function makeRequest(msg) {
+  const length = msg.length || msg.byteLength;
+  const data = new Uint8Array(length + 4)
+
+  const reqIdHeader = new Uint8Array(4);
+  window.crypto.getRandomValues(reqIdHeader);
+  // the first bit HAS TO BE one, in order to get a response
+  reqIdHeader[0] |= 1 << 7;
+
+  data.set(reqIdHeader, 0);
+
+  if (typeof msg === 'string' || msg instanceof String) {
+    for (let i = 4; i < msg.length + 4; ++i) {
+      data[i] = msg.charCodeAt(i - 4);
+    }
+
+  } else {
+    data.set(msg, 4);
+  }
+
+  repsocket.send(data)
+
+  let dataview = new DataView(reqIdHeader.buffer)
+  console.log('REQ #', dataview.getUint32(0), ': ', msg)
+}
+
+
+subsocket.onerror = (event) => {
+  console.log('SUB error')
   console.log(event)
 }
 
-socket.onopen = (event) => {
-  console.log('open')
+subsocket.onopen = (event) => {
+  console.log('SUB open')
   console.log(event)
 }
 
-socket.onmessage = (event) => {
-  console.log(Date.now(), 'message')
+subsocket.onmessage = (event) => {
+  console.log(Date.now(), 'SUB message')
   console.log(event)
+  console.log("Message: ", dec.decode(event.data))
+}
+
+repsocket.onerror = (event) => {
+  console.log('REP error')
+  console.log(event)
+}
+
+repsocket.onopen = (event) => {
+  console.log('REP open')
+  console.log(event)
+}
+
+repsocket.onmessage = (event) => {
+  console.log(Date.now(), 'REP message')
+  console.log(event)
+  let dataview = new DataView(event.data.slice(0,4))
+  console.log("REP #", dataview.getUint32(0), ": ", dec.decode(event.data.slice(4)))
 }
